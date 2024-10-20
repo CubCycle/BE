95db5400cc0cfb9690a0f12cf4a87e306c8d166f
minjgziii <kmj4668@gmail.com>
2024-10-20 20:09:31 +0900

    feat #9 : 카페의 다회용컵 재고 조회 Service 코드 작성

--- src/main/java/com/example/cupcycle/service/CafeService.java
+++ src/main/java/com/example/cupcycle/service/CafeService.java
@@ -1,4 +1,29 @@
 package com.example.cupcycle.service;
 
+import com.example.cupcycle.entity.Cafe;
+import com.example.cupcycle.repository.CafeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CafeService {
+    private final CafeRepository cafeRepository;
+
+    @Autowired
+    public CafeService(CafeRepository cafeRepository) {
+        this.cafeRepository = cafeRepository;
+    }
+
+    /*
+     * 카페의 다회용컵 재고 상태 조회
+     */
+    public int getAvailableCups(int id) {
+        Cafe cafe = cafeRepository.findByCafeId(id);
+
+        if (cafe == null) {
+            return 0;
+        }
+
+        return cafe.getAvailableCups();
+    }
 }
