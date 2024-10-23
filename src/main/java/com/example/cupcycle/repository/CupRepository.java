e680ac2fe52e89f3f3237f485e4b9034ddd9f47b
INJOON CHOI <dlswns8679@naver.com>
2024-10-23 11:09:46 +0900

    refactor - 캐시 삭제

--- src/main/java/com/example/cupcycle/repository/CupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cupcycle.repository;
-
-import com.example.cupcycle.entity.Cup;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CupRepository extends JpaRepository<Cup, Integer> {
-}
--- src/test/java/com/example/cupcycle/service/CupServiceTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.cupcycle.service;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class CupServiceTest {
-
-}
